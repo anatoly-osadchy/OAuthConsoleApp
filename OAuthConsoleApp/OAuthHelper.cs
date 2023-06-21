7dc86a8986400317bbca7eb04ed3b601635afe2d
Anatolii Osadchyi <anatolii.osadchyi@3shape.com>
2023-06-21 14:55:31 +0200

    Add project files.

---
+++ OAuthConsoleApp/OAuthHelper.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OAuthConsoleApp
+{
+    public class OAuthRequestValue
+    {
+        public OAuthRequestValue(string key, string value = null)
+        {
+            Key = key;
+            Value = value;
+        }
+
+        public string Key { get; set; }
+        public string Value { get; set; }
+
+        public bool IsValid => !Key.IsNullOrEmpty() && !Value.IsNullOrEmpty();
+    }
+
+    public class OAuthRequestCodeParams
+    {
+        public OAuthRequestValue RedirectUri { get; set; } = new("redirect_uri");
+        public OAuthRequestValue ResponseType { get; set; } = new("response_type", "code");
+        public OAuthRequestValue Scope { get; set; } = new("scope");
+        public OAuthRequestValue ClientId { get; set; } = new("client_id");
+        public OAuthRequestValue State { get; set; } = new("state");
+
+        public string ResponseCodeKey { get; set; } = "code";
+        public string ResponseStateKey { get; set; } = "state";
+
+        public IEnumerable<OAuthRequestValue> Values => new[]
+        {
+            RedirectUri,
+            ResponseType,
+            Scope,
+            ClientId,
+            State
+        };
+    }
+
+    public class OAuthRequestTokenParams
+    {
+        public OAuthRequestValue RedirectUri { get; set; } = new("redirect_uri");
+        public OAuthRequestValue Code { get; set; } = new("code");
+        public OAuthRequestValue ClientId { get; set; } = new("client_id");
+        public OAuthRequestValue ClientSecret { get; set; } = new("client_secret");
+        public OAuthRequestValue CodeVerifier { get; set; } = new("code_verifier");
+        public OAuthRequestValue Scope { get; set; } = new("scope");
+        public OAuthRequestValue GrantType { get; set; } = new("grant_type", "authorization_code");
+
+        public string ResponseAccessTokenKey { get; set; } = "access_token";
+        public string ResponseRefreshTokenKey { get; set; } = "refresh_token";
+        public string ResponseExpirationKey { get; set; } = "expiration";
+
+        public IEnumerable<OAuthRequestValue> Values => new[]
+        {
+            RedirectUri,
+            Code,
+            ClientId,
+            ClientSecret,
+            CodeVerifier,
+            Scope,
+            GrantType
+        };
+    }
+
+    public class OAuthToken
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime? Expiration { get; set; }
+    }
+
+    public class OAuthHelper
+    {
+        public OAuthRequestCodeParams RequestCodeParams { get; } = new();
+        public OAuthRequestTokenParams RequestTokenParams { get; } = new();
+
+        public Uri RequestCodeUrl { get; set; } = new("https://www.googleapis.com/oauth2/v4/token");
+        public Uri RequestTokenUrl { get; set; } = new("https://accounts.google.com/o/oauth2/v2/auth");
+        public string FinishHtmlCode { get; set; } = "<html><head><meta http-equiv='refresh' content='10;url=https://google.com'></head><body>Please return to the app.</body></html>";
+
+        public async Task<OAuthToken> Run()
+        {
+            var httpServerTask = StartHttpServer();
+
+            RequestCode();
+
+            var code = await httpServerTask;
+
+            return await RequestToken(code);
+        }
+
+        private void RequestCode()
+        {
+            var state = GenerateRandomDataBase64url(32);
+            RequestCodeParams.State.Value = state;
+
+            var urlBuilder = new UriBuilder(RequestCodeUrl);
+            urlBuilder.Query = RequestCodeParams.Values
+                .Where(i => i.IsValid)
+                .Select(i => $"{i.Key}={Uri.EscapeDataString(i.Value)}")
+                .JoinString("&");
+
+            // Creates the OAuth 2.0 authorization request.
+            var request = urlBuilder.ToString();
+
+            // Opens request in the browser.
+            var pi = new ProcessStartInfo(request, "");
+            pi.UseShellExecute = true;
+            Process.Start(pi);
+        }
+
+        private async Task<OAuthToken> RequestToken(string code)
+        {
+            RequestTokenParams.Code.Value = code;
+            RequestTokenParams.RedirectUri.Value = RequestCodeParams.RedirectUri.Value;
+
+            // ???
+            RequestTokenParams.CodeVerifier.Value = GenerateRandomDataBase64url(32);
+
+            var tokenRequest = new HttpClient();
+
+            var formValues = RequestTokenParams.Values
+                .Where(i => i.IsValid)
+                .ToDictionary(i => i.Key, i => Uri.EscapeDataString(i.Value));
+
+            var content = new FormUrlEncodedContent(formValues);
+
+            var tokenResponse = await tokenRequest.PostAsync(RequestTokenUrl, content);
+
+            //var responseText = await tokenResponse.Content.ReadAsStringAsync();
+            var response = await tokenResponse.Content.ReadFromJsonAsync<Dictionary<string, string>>();
+            if (response == null)
+            {
+                throw new Exception("Invalid response");
+            }
+
+            return new OAuthToken
+            {
+                AccessToken = GetValue(RequestTokenParams.ResponseAccessTokenKey, true, i => i),
+                RefreshToken = GetValue(RequestTokenParams.ResponseRefreshTokenKey, true, i => i),
+                Expiration = GetValue(RequestTokenParams.ResponseExpirationKey, true, ToDateTime)
+            };
+
+            T? GetValue<T>(string key, bool mandatory, Func<string, T?> parser)
+            {
+                if (response.TryGetValue(key, out var value))
+                {
+                    return parser(value);
+                }
+
+                if (mandatory)
+                {
+                    throw new Exception($"Request for token didn't return '{key}' value");
+                }
+
+                return default;
+            }
+
+            DateTime? ToDateTime(string str)
+            {
+                // TODO implement parser
+                return null;
+            }
+        }
+
+        private async Task<string> StartHttpServer()
+        {
+            // Creates a redirect URI using an available port on the loopback address.
+            string redirectUri = $"http://{IPAddress.Loopback}:{GetRandomUnusedPort()}/";
+
+            // Creates an HttpListener to listen for requests on that redirect URI.
+            var http = new HttpListener();
+            http.Prefixes.Add(redirectUri);
+            http.Start();
+
+            // Waits for the OAuth authorization response.
+            var context = await http.GetContextAsync();
+
+            // Sends an HTTP response to the browser.
+            var response = context.Response;
+            var buffer = Encoding.UTF8.GetBytes(FinishHtmlCode);
+            response.ContentLength64 = buffer.Length;
+            var responseOutput = response.OutputStream;
+            await responseOutput.WriteAsync(buffer, 0, buffer.Length);
+            responseOutput.Close();
+            http.Stop();
+
+            string error = context.Request.QueryString.Get("error");
+            if (error is not null)
+            {
+                // process error
+                throw new Exception(error);
+            }
+
+            // extracts the code + state
+            var code = context.Request.QueryString.Get(RequestCodeParams.ResponseCodeKey);
+            if (code.IsNullOrEmpty())
+            {
+                throw new Exception("Code is empty");
+            }
+
+            var state = context.Request.QueryString.Get(RequestCodeParams.ResponseStateKey);
+            var stateIsRequested = RequestCodeParams.State.IsValid;
+            if (stateIsRequested && state.IsNullOrEmpty())
+            {
+                // process error
+                throw new Exception("State is not received");
+            }
+
+            return code;
+        }
+
+        // ref http://stackoverflow.com/a/3978040
+        public static int GetRandomUnusedPort()
+        {
+            var listener = new TcpListener(IPAddress.Loopback, 0);
+            listener.Start();
+            var port = ((IPEndPoint)listener.LocalEndpoint).Port;
+            listener.Stop();
+            return port;
+        }
+
+        /// <summary> Returns URI-safe data with a given input length. </summary>
+        private static string GenerateRandomDataBase64url(int length)
+        {
+            byte[] bytes = RandomNumberGenerator.GetBytes(length);
+            return Base64UrlEncodeNoPadding(bytes);
+        }
+
+        /// <summary> Base64url no-padding encodes the given input buffer. </summary>
+        private static string Base64UrlEncodeNoPadding(byte[] buffer)
+        {
+            string base64 = Convert.ToBase64String(buffer);
+
+            // Converts base64 to base64url.
+            base64 = base64.Replace("+", "-");
+            base64 = base64.Replace("/", "_");
+            // Strips padding.
+            base64 = base64.Replace("=", "");
+
+            return base64;
+        }
+    }
+
+    static class StringExtensions
+    {
+        public static bool IsNullOrEmpty(this string str)
+        {
+            return string.IsNullOrEmpty(str);
+        }
+
+        public static string JoinString(this IEnumerable<string> strings, string separator = ",")
+        {
+            return strings != null ? string.Join(separator, strings) : null;
+        }
+    }
+}
---
+++ OAuthConsoleApp/Program.cs
@@ -0,0 +1,287 @@
+// Copyright 2016 Google Inc.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OAuthConsoleApp
+{
+    class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            //if (args.Length != 2)
+            //{
+            //    Console.WriteLine("Required command line arguments: client-id client-secret");
+            //    return 1;
+            //}
+            string clientId = "1084886105350-60j8b8mgifkpvet5t798io42dtcgmgeb.apps.googleusercontent.com";
+            string clientSecret = "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";
+
+            Console.WriteLine("+-----------------------+");
+            Console.WriteLine("|  Sign in with Google  |");
+            Console.WriteLine("+-----------------------+");
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to sign in...");
+            //Console.ReadKey();
+
+            await DoOAuthAsync(clientId, clientSecret);
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+            return 0;
+        }
+
+        // ref http://stackoverflow.com/a/3978040
+        public static int GetRandomUnusedPort()
+        {
+            var listener = new TcpListener(IPAddress.Loopback, 0);
+            listener.Start();
+            var port = ((IPEndPoint)listener.LocalEndpoint).Port;
+            listener.Stop();
+            return port;
+        }
+
+        private static async Task DoOAuthAsync(string clientId, string clientSecret)
+        {
+            // Generates state and PKCE values.
+            string state = GenerateRandomDataBase64url(32);
+            string codeVerifier = GenerateRandomDataBase64url(32);
+            const string AuthorizationEndpoint = "https://accounts.google.com/o/oauth2/v2/auth";
+
+            // Creates a redirect URI using an available port on the loopback address.
+            string redirectUri = $"http://{IPAddress.Loopback}:{GetRandomUnusedPort()}/";
+            Log("redirect URI: " + redirectUri);
+
+            // Creates an HttpListener to listen for requests on that redirect URI.
+            var http = new HttpListener();
+            http.Prefixes.Add(redirectUri);
+            Log("Listening..");
+            http.Start();
+
+            (string key, string value)[] queryParts =
+                {
+                    ("redirect_uri", redirectUri),
+                    ("response_type", "code"),
+                    ("scope", "openid https://www.googleapis.com/auth/userinfo.email"),
+                    ("client_id", clientId),
+                    ("state", state),
+                };
+            var urlBuilder = new UriBuilder(AuthorizationEndpoint);
+            urlBuilder.Query = queryParts
+                .Select(i => $"{i.key}={Uri.EscapeDataString(i.value)}")
+                .JoinString("&");
+
+
+            // Creates the OAuth 2.0 authorization request.
+            // Opens request in the browser.
+            var request = urlBuilder.ToString();
+            Log($"Request: {request}");
+            var pi = new ProcessStartInfo(request, "");
+            pi.UseShellExecute = true;
+            Process.Start(pi);
+
+            // Waits for the OAuth authorization response.
+            var context = await http.GetContextAsync();
+
+            // Brings the Console to Focus.
+            BringConsoleToFront();
+
+            // Sends an HTTP response to the browser.
+            var response = context.Response;
+            string responseString = "<html><head><meta http-equiv='refresh' content='10;url=https://google.com'></head><body>Please return to the app.</body></html>";
+            byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+            response.ContentLength64 = buffer.Length;
+            var responseOutput = response.OutputStream;
+            await responseOutput.WriteAsync(buffer, 0, buffer.Length);
+            responseOutput.Close();
+            http.Stop();
+            Log("HTTP server stopped.");
+
+            // Checks for errors.
+            string error = context.Request.QueryString.Get("error");
+            if (error is not null)
+            {
+                Log($"OAuth authorization error: {error}.");
+                return;
+            }
+            if (context.Request.QueryString.Get("code") is null
+                || context.Request.QueryString.Get("state") is null)
+            {
+                Log($"Malformed authorization response. {context.Request.QueryString}");
+                return;
+            }
+
+            // extracts the code
+            var code = context.Request.QueryString.Get("code");
+            var incomingState = context.Request.QueryString.Get("state");
+
+            // Compares the received state to the expected value, to ensure that
+            // this app made the request which resulted in authorization.
+            if (incomingState != state)
+            {
+                Log($"Received request with invalid state ({incomingState})");
+                return;
+            }
+            Log("Authorization code: " + code);
+
+            // Starts the code exchange at the Token Endpoint.
+            await ExchangeCodeForTokensAsync(code, codeVerifier, redirectUri, clientId, clientSecret);
+        }
+
+        private static async Task ExchangeCodeForTokensAsync(string code, string codeVerifier, string redirectUri, string clientId, string clientSecret)
+        {
+            Log("Exchanging code for tokens...");
+
+            // builds the  request
+            string tokenRequestUri = "https://www.googleapis.com/oauth2/v4/token";
+
+            // sends the request
+            var tokenRequest = new HttpClient();
+            //tokenRequest.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/html"));
+            //tokenRequest.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xhtml+xml"));
+            //tokenRequest.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml;q=0.9"));
+            //tokenRequest.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("*/*;q=0.8"));
+
+            try
+            {
+                // gets the response
+                var content = new FormUrlEncodedContent(new Dictionary<string, string>
+                {
+                    { "code", code },
+                    { "redirect_uri", Uri.EscapeDataString(redirectUri) },
+                    { "client_id", clientId },
+                    { "code_verifier", codeVerifier },
+                    { "client_secret", clientSecret },
+                    { "scope", "" },
+                    { "grant_type", "authorization_code" }
+                });
+                var tokenResponse = await tokenRequest.PostAsync(new Uri(tokenRequestUri), content);
+                var responseText = await tokenResponse.Content.ReadAsStringAsync();
+                Console.WriteLine(responseText);
+
+                // converts to dictionary
+                var response = await tokenResponse.Content.ReadFromJsonAsync<Dictionary<string, string>>();
+
+                string accessToken = response!["access_token"];
+                await RequestUserInfoAsync(accessToken);
+            }
+            catch (HttpRequestException ex)
+            {
+                Log($"HTTP: {ex.StatusCode}\r\nException: {ex}");
+            }
+        }
+
+        private static async Task RequestUserInfoAsync(string accessToken)
+        {
+            Log("Making API Call to UserInfo...");
+
+            // builds the  request
+            string userInfoRequestUri = "https://www.googleapis.com/oauth2/v3/userinfo";
+
+            // sends the request
+            var userInfoRequest = new HttpClient();
+            userInfoRequest.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            //userInfoRequest.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/html"));
+            //userInfoRequest.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xhtml+xml"));
+            //userInfoRequest.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml;q=0.9"));
+            //userInfoRequest.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("*/*;q=0.8"));
+
+            // gets the response
+            var userInfoResponse = await userInfoRequest.GetAsync(new Uri(userInfoRequestUri));
+            var userInfoResponseText = await userInfoResponse.Content.ReadAsStringAsync();
+            Log(userInfoResponseText);
+        }
+
+        /// <summary> Appends the given string to the on-screen log, and the debug console. </summary>
+        private static void Log(string output, ConsoleColor? fgColor = null, ConsoleColor? bgColor = null)
+        {
+            var colors = (fg: Console.ForegroundColor, bg: Console.BackgroundColor);
+            if (fgColor != null || bgColor != null)
+            {
+                Console.ForegroundColor = fgColor ?? colors.fg;
+                Console.BackgroundColor = bgColor ?? colors.bg;
+            }
+
+            Console.WriteLine(output);
+
+            (Console.ForegroundColor, Console.BackgroundColor) = colors;
+        }
+
+        /// <summary>
+        /// Returns URI-safe data with a given input length.
+        /// </summary>
+        /// <param name="length">Input length (nb. output will be longer)</param>
+        /// <returns></returns>
+        private static string GenerateRandomDataBase64url(int length)
+        {
+            byte[] bytes = RandomNumberGenerator.GetBytes(length);
+            return Base64UrlEncodeNoPadding(bytes);
+        }
+
+        /// <summary>
+        /// Returns the SHA256 hash of the input string, which is assumed to be ASCII.
+        /// </summary>
+        private static byte[] Sha256Ascii(string text)
+        {
+            var bytes = Encoding.ASCII.GetBytes(text);
+            using var sha256 = SHA256.Create();
+            return sha256.ComputeHash(bytes);
+        }
+
+        /// <summary>
+        /// Base64url no-padding encodes the given input buffer.
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <returns></returns>
+        private static string Base64UrlEncodeNoPadding(byte[] buffer)
+        {
+            string base64 = Convert.ToBase64String(buffer);
+
+            // Converts base64 to base64url.
+            base64 = base64.Replace("+", "-");
+            base64 = base64.Replace("/", "_");
+            // Strips padding.
+            base64 = base64.Replace("=", "");
+
+            return base64;
+        }
+
+        // Hack to bring the Console window to front.
+        // ref: http://stackoverflow.com/a/12066376
+
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        public static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        private static void BringConsoleToFront()
+        {
+            SetForegroundWindow(GetConsoleWindow());
+        }
+    }
+}
